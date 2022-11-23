065df2dede4fc128595552ad755b9c2281dd6fab
陈星星 <chenxingxing@shulie.io>
2022-11-23 11:55:07 +0800

    Initial commit

---
+++ src/main/java/io/shulie/vocabularyengine/VocabularyEngineApplication.java
@@ -0,0 +1,13 @@
+package io.shulie.vocabularyengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabularyEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/shulie/vocabularyengine/VocabularyEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shulie.vocabularyengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
